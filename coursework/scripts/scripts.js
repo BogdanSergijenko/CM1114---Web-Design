43898343c36b2cbcf44438c178af3d45a0fcbc0c
BogdanSergijenko <146110009+BogdanSergijenko@users.noreply.github.com>
2024-04-01 18:53:59 +0100

    Update scripts.js
    Updated to final version

--- coursework/scripts/scripts.js
+++ coursework/scripts/scripts.js
@@ -1,16 +1,21 @@
+//Assigns basic variable for car clowns
 let clownwidth = 0;
 let clownheight = 0;
 
 function car() {
+    //Checks if clown limit has been reached
     if (clownheight < 100){
+        //Creates an elements and assigns it an image
         let img = document.createElement("img");
         img.src = "images/ClownCarClowns.gif";
 
+        //Sets the CSS for the element
         img.setAttribute("class", "carclowns");
         img.style.left = clownwidth + "%";
         img.style.bottom = clownheight + "%";
         document.body.appendChild(img);
 
+        //Alters the position of the next clown
         if (clownwidth < 90){
             clownwidth += 10;
         } else {
@@ -20,20 +25,29 @@ function car() {
     }
 }
 
+
+//Plays a honk sound
 function honk(){
     var x = document.getElementById("honksound");
     x.play();
 }
 
+
+//Updates text for contact us form
 function send_form(id,text){
     document.getElementById(id).innerHTML=text;
 }
 
+
+//Variable for login function
 logincounter = 0;
 
 function login(){
+    //Gets username and password from the login form
     var username = document.forms["login"]["username"].value;
     var password = document.forms["login"]["password"].value;
+
+    //Validates if the username and password are entered
     if (username == "") {
         alert( "Must enter username!");
     } else {
@@ -42,6 +56,7 @@ function login(){
         } else {
             logincounter++;
 
+            //Sends and alternating message if validation successful
             if (logincounter%2 == 1){
                 alert("You have been logged in!");
             } else {
@@ -51,20 +66,24 @@ function login(){
     }
 }
 
+
+//Unblurs and gives a border to an image
 function highlight(image) {
     image.style="border: solid blue 2px; filter: none;";
 }
+
+//Resets the border of an image
 function delight(image) {
     image.style="border: none;";
 }
 
-function unblur(image){
-    image.style="filter: none;";
-}
 
 function submit_event() {
+    //Gets username and password from the event form
     var title = document.forms["eventForm"]["event_title"].value;
     var date = document.forms["eventForm"]["event_date"].value;
+
+    //Validates if title and date have been entered
     if (title == "") {
         alert( "An event must have a title!");
     } else {
@@ -74,4 +93,4 @@ function submit_event() {
             alert("Event submitted! The society will contact you soon!");
         }
     }
-}    
+}   
